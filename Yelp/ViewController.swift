1cf033f69bdae386ca7786bd75999ab6465cc44d
Harsha Vashisht <rvharsha@gmail.com>
2014-09-23 08:11:53 -0700

    Initial Commit

---
+++ Yelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yelp
+//
+//  Created by Harsha Vashisht on 9/23/14.
+//  Copyright (c) 2014 Harsha Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
